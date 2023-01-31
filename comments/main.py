370379438844a1aec804cfef0c0ff06d4635da0d
F van limburg <101403517+5PE3DY@users.noreply.github.com>
2023-01-31 11:52:41 +0100

    Add files via upload
    Files for exercises python

---
+++ comments/main.py
@@ -0,0 +1,36 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+"""When you write code, it's very common to add some notes to human readers in your code. Almost every programming language has a way of including these "comments". Comments are pieces of text that the computer does not look at when running a program. They are only meant for the human readers of code.
+
+Each language has their own way, or ways, of adding comments. Each way has its own advantages and disadvantages, for now just use what looks nice to you. Python has the following ways to add comments:
+
+# Here's a single-line comment.
+answer = 42
+question = "How many roads?" # Here's an end-of-line comment."""
+
+""" Here's a multiline comment.
+It uses multiple lines and is the only comment type that we need to
+terminate explicitly. """
+"""print(question, answer)
+Read the following article on comments in Python. This article also has information on what you should and should not write in comments: stackabuse.com -- commenting Python code(opens in a new tab)
+
+Exercise
+
+1
+
+1
+Say yes more. Yes opens doors. No closes them. Yes pushes us. No keeps us safe at home. Imagine all the opportunities waiting for a yes.
+
+2
+
+2
+Let go of expectation. You’ve done your best to prepare. Now, let go. There’s no one way your life should unfold. Enjoy the journey.
+
+3
+
+3
+Welcome diversions. The most rewarding adventures often start with an unexpected detour. Perhaps that distraction will guide you onward."""
---
+++ variables/main.py
@@ -0,0 +1,16 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+names = "Spaghetti, pizza"
+print(names)
+
+age = 73
+print(age)
+
+
+a = 18
+b = 21
+bool = a < b
+print(bool)
