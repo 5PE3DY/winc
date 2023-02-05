a8d3f5b3e678ccfc34d0c1db58c0104cd5727602
F van limburg <101403517+5PE3DY@users.noreply.github.com>
2023-02-05 14:47:43 +0100

    Add files via upload
    New files included for the python exercises

---
+++ functions/main.py
@@ -0,0 +1,22 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+#1 Greet
+
+def greet(name):
+    return f"Hello, {name}!"
+
+#2 Add
+def add(a, b, c):
+    return a + b + c
+
+#3 positive
+def positive(x):
+    return x > 0
+
+#4 negative
+def negative(x):
+    return x < 0
+    
